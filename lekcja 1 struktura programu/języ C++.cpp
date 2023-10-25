ed4f530bfbd9694474e962268c16b5bc4bb126ee
Badylek123456 <mrogalska582@technikumkreatywne.pl>
2023-10-25 11:56:00 +0200

    Komentarz

--- lekcja 1 struktura programu/języ C++.cpp
+++ lekcja 1 struktura programu/języ C++.cpp
@@ -1,9 +1,26 @@
  #include <iostream>
  using namespace std;
+ 
+ 
  int main(){
- 	
-cout << "Hello world" <<endl;
-cout << "Imie";
+  short liczba = 2;   // zmienna liczbowa
+    float liczbaUzytkownika;	
+    cout << "Hello world" <<endl;
+    cout << "Imie" << liczba;
+    
+    cout << endl;
+    cout << "Podaj liczbe";
+    cin >> liczbaUzytkownika; // wprowadzanie danych i zapisanie do zmiennej
+    
+    
+    cout << endl;
+    cout << "Podana liczba: " << liczbaUzytkownika;
+    /*
+	Komentarz
+	na wiele lini
+	
+	*/
+    
 
 
 
