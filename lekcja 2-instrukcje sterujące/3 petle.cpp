b8b27096f885fb93a9238f64257b283439e8e0cd
Badylek123456 <mrogalska582@technikumkreatywne.pl>
2023-11-08 11:57:55 +0100

    KOmentarz

---
+++ lekcja 2-instrukcje sterujące/3 petle.cpp
@@ -0,0 +1,32 @@
+   #include <iostream>
+   using namespace std;
+   
+   int main(){
+   	    /*Inkrementacja
+   	     i = i + 1
+   	     i += 1
+   	     i++
+   	    Denkramentacja
+   	     i = i - 1
+   	     i -= 1
+   	     i--
+		*/
+		// for(wartoscPoczatkowa; warunekKanczacy; krok)
+		for(int i = 0; i < 5; i++){ // i - zmienna lokalna, istnieje tylko w petli
+			cout << "Iteracja nr: " << i << endl;
+		}
+   	    
+   	    // zadac pytanie
+   	    // pobrac odpowiedz
+   	    // zadac pytanie
+   	    // pobrac odpowiedz
+   	    
+        for(int i= 0; i < 5; i++){
+        	cout << " " << i << j;
+        	
+		}
+   	
+   	
+   	cout << endl;
+   	system("pause");
+   }
